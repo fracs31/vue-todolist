ceb5107401d77a6a436dc5f224b356bb24ed0ba1
fracs31 <pist.fracs95@gmail.com>
2023-02-21 14:28:31 +0100

    ADD array of tasks

--- js/app.js
+++ js/app.js
@@ -3,7 +3,29 @@ const { createApp } = Vue; //Vue
 createApp({
     data() {
       return {
-        
+        //Tasks
+        tasks: [
+            {
+                text: "Svuotare l'armadio", //testo
+                done: true //task svolta
+            },
+            {
+                text: "Pulire l'armadio", //testo
+                done: true //task svolta
+            },
+            {
+                text: "Riorganizzare l'armadio", //testo
+                done: false //task svolta
+            },
+            {
+                text: "Rimettere tutto nell'armadio", //testo
+                done: false //task svolta
+            },
+            {
+                text: "Dare via i vestiti che non si usano più", //testo
+                done: false //task svolta
+            }
+        ]
       }
     }
 }).mount('#app');
